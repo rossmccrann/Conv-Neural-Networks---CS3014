388b28e7d84fb2527beebd274dc0ea3514ce40cd
rossmccrann <rossmccrann@users.noreply.github.com>
2020-04-09 17:48:31 +0100

    testing

--- conv-harness.c
+++ conv-harness.c
@@ -501,7 +501,7 @@ void team_conv_sparse(float *** image, struct sparse_matrix *** kernels,
   for ( m = 0; m < nkernels; m++ ) {
     for ( h = 0; h < height; h++ ) {
       for ( w = 0; w < width; w++ ) {
-	       double check = output[m][h][w] ;
+	       char check = output[m][h][w] ;
          printf(check );
       }
     }
